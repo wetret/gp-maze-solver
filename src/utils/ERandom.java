09904bfd9e7e5535f07d353abb6242d0525c6f6a
Reto Wettstein <retowettstein@hotmail.com>
2015-07-14 15:10:58 +0200

    add utils with random class

---
+++ src/utils/ERandom.java
@@ -0,0 +1,19 @@
+package utils;
+
+import java.util.Random;
+
+
+public enum ERandom {
+    
+    INSTANCE(new Random());
+
+    private Random mRandom;
+
+    private ERandom(Random pRandom) {
+        mRandom = pRandom;
+    }
+
+    public Random getRandom(){
+        return mRandom;
+    }
+}
