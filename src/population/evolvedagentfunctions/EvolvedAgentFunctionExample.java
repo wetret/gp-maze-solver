1336c1d44c1f08059830b220752ce6a0cac1f5c7
Reto Wettstein <retowettstein@hotmail.com>
2015-07-16 12:56:11 +0200

    implemented 1 agent function for testing

--- src/population/evolvedagentfunctions/EvolvedAgentFunctionExample.java
+++ src/population/evolvedagentfunctions/EvolvedAgentFunctionExample.java
@@ -12,7 +12,21 @@ public class EvolvedAgentFunctionExample implements IMoveNode{
         // add the generated agent function from GeneticProgramming.evolve() here.
         // remove "return null"
         
-        return null;
+        if(pMaze.isWallWest() && pMaze.isWallNorth()){
+            return Move.EAST;
+        } else if(pMaze.isWallWest()) {
+            return Move.NORTH;
+        } else if(pMaze.isWallNorth() && pMaze.isWallEast()) {
+            return Move.SOUTH;
+        } else if(!pMaze.isWallWest() && !pMaze.isWallNorth() && pMaze.isWallNorthWest()) {
+            return Move.NORTH;
+        } else if(pMaze.isWallEast()) {
+            return Move.SOUTH;
+        } else {
+            return Move.EAST;
+        }
+        
+        // return null;
     }
 
     @Override
