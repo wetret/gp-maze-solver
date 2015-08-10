87a0352a8284b8fedca8a788c6b9ed16ee26f9c6
Reto Wettstein <retowettstein@hotmail.com>
2015-08-10 16:48:54 +0200

    update controll package

--- src/controll/Main.java
+++ src/controll/Main.java
@@ -12,53 +12,56 @@ import utils.Config;
 
 
 public class Main {
-    
-    public static void main(String args[]){
-        if(args.length == 2 && args[0].equals("show") && Integer.parseInt(args[1]) > 0 && Integer.parseInt(args[1]) < 7){  
+
+    public static void main(String args[]) {
+        if (args.length == 2 && args[0].equals("show") && Integer.parseInt(args[1]) > 0 && Integer.parseInt(args[1]) < 7) {
             withWindow(Integer.parseInt(args[1]));
-        } else if(args.length == 4 && args[0].equals("evolve") && Integer.parseInt(args[2]) > 0 && Integer.parseInt(args[2]) < 4 && Integer.parseInt(args[3]) > 0 && Integer.parseInt(args[3]) < 7 ) {
+        } else if (args.length == 4 && args[0].equals("evolve") && Integer.parseInt(args[2]) > 0 && Integer.parseInt(args[2]) < 4 
+                && Integer.parseInt(args[3]) > 0 && Integer.parseInt(args[3]) < 7) {
             geneticProgramming(Integer.parseInt(args[1]), Integer.parseInt(args[2]), Integer.parseInt(args[3]));
         } else {
             System.out.println("Wrong input arguments, use: \n"
                     + "- show <mazenumber> where the mazenumber is between 1 and 6 \n"
-                    + "- evolve <populationsize> <fitnessfunction> <mazenumber> where the fitnessfunction is between 1 and 3 and the mazenumber as above.");
+                    + "- evolve <populationsize> <fitnessfunction> <mazenumber> where the fitnessfunction "
+                    + "is between 1 and 3 and the mazenumber as above.");
         }
     }
-    
+
     private static void geneticProgramming(int pPopulationSize, int pFitnessFunction, int pMazeNumber) {
-       Agent best = new GeneticProgramming(pPopulationSize, pFitnessFunction, pMazeNumber).evolve();
-       System.out.println("\nBest Fitness: " + best.getFitness() + "\nEvolved Agent Function: " + best.getEvaluationTree().evaluationToString());
+        Agent best = new GeneticProgramming(pPopulationSize, pFitnessFunction, pMazeNumber).evolve();
+        System.out.println("\nBest Fitness: " + best.getFitness() + "\nEvolved Agent Function: " + best.getEvaluationTree().evaluationToString());
     }
-    
-    private static void withWindow(int mMazeNumber){
+
+    private static void withWindow(int mMazeNumber) {
         JFrame top = new JFrame("Maze");
-        top.setBounds(Config.DEFAULT.getXCord(), Config.DEFAULT.getYCord(), Config.DEFAULT.getPixelSize() * Config.DEFAULT.getGridWidth(), Config.DEFAULT.getPixelSize() * Config.DEFAULT.getGridHeight() + 22);
-            
+        top.setBounds(Config.DEFAULT.getXCord(), Config.DEFAULT.getYCord(), Config.DEFAULT.getPixelSize() * Config.DEFAULT.getGridWidth(), 
+                Config.DEFAULT.getPixelSize() * Config.DEFAULT.getGridHeight() + 22);
+
         top.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         top.setResizable(true);
-        
+
         IMoveNode root = new EvolvedAgentFunctionOne();
-        
+
         Maze maze = new Maze(mMazeNumber);
         Agent agent = new Agent(root, maze);
-        
+
         Window window = new Window(maze);
         top.getContentPane().add(window);
         top.setVisible(true);
-        
+
         boolean notReachedGoal = true;
-        while(notReachedGoal){
+        while (notReachedGoal) {
             notReachedGoal = agent.move();
-                
+
             try {
                 Thread.sleep(100);
             } catch (InterruptedException pEx) {
                 pEx.printStackTrace();
             }
-            
+
             top.repaint();
         }
-        
+
         System.out.println("Number of steps needed to find the goal: " + agent.getStepsTaken());
     }
 }
